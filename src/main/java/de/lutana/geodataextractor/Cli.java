7da53f71f67cf0b6cac46cb0263a6c4f8bcf49b6
Matthias Mohr <webmaster@mamo-net.de>
2017-06-07 13:16:21 +0200

    Basic IDE setup, including openCV and pdffigures dependency.

---
+++ src/main/java/de/lutana/geodataextractor/Cli.java
@@ -0,0 +1,14 @@
+package de.lutana.geodataextractor;
+
+/**
+ * Command Line Interface for the Geodata Extractor.
+ * 
+ * @author Matthias Mohr
+ */
+public class Cli {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
